069fc519da26b102f8b935b9d279be5234db6741
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-04-17 14:00:56 +0300

    fix

--- remote.js
+++ remote.js
@@ -5,17 +5,17 @@ const express = require('express');
 const app = express();
 
 module.exports = (app, httpPort) => {
-app.enable('trust proxy');
+    app.enable('trust proxy');
 
+    app.use ((req, res, next) => {
+        if (req.secure) {
+            next();
+        } else {
+            const proxypath = process.env.PROXY_PASS || ''
+            res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
+        }
+    });
 
-app.use ((req, res, next) => {
-    if (req.secure) {
-        next();
-    } else {
-        const proxypath = process.env.PROXY_PASS || ''
-
-        res.redirect(301, `https://${req.headers.host}${proxypath}${req.url}`);
-    }
-});
-app.listen(httpPort);
+    app.listen(httpPort);
 };
+
