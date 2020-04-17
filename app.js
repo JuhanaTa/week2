15b48978b9bfdf0b5e41d6418d01cb462f2e51e8
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-04-17 12:02:24 +0300

    CentOs rdy

--- app.js
+++ app.js
@@ -2,31 +2,28 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
 const app = express();
 const port = 3000;
-const httpsPort = 8000;
 
-const sslkey = fs.readFileSync('ssl-key.pem');
-const sslcert = fs.readFileSync('ssl-cert.pem')
-
-const options = {
-    key: sslkey,
-    cert: sslcert
-};
 
 const passport = require('./utils/pass');
 const authRoute = require('./routes/authRoute');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
 
+
 app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static('week2_public_html'));
 app.use(express.static('uploads'));
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+    require('./production')(app, process.env.PORT);
+} else {
+    require('./localhost')(app, port);
+}
 
 app.use('/auth', authRoute);
 app.use('/cat',passport.authenticate('jwt', {session: false}), catRoute);
@@ -36,9 +33,7 @@ app.get('/secure', (req, res) => {
     res.send(`Hello Secure World! ${req.secure}`);
 });
 
+
+
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-http.createServer((req, res) => {
-    res.writeHead(301, { 'Location': 'https://localhost:8000' + req.url });
-    res.end();
-}).listen(port);
-https.createServer(options, app).listen(httpsPort);
+
